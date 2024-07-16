4e38fa95245d3da2fb6fc86c9279a867a3cbde3f
DibCadeira <touchstart-@outlook.com>
2024-07-16 17:13:13 -0300

    added methods add, remove, set_data to NodeStack

--- src/core/nodestack.py
+++ src/core/nodestack.py
@@ -1,7 +1,19 @@
+from src.core.factory import Factory
+
+
 class NodeStack:
     def __init__(self):
         self.nodes = []
         self.stack = []
+
+    def add(self, node: str):
+        self.nodes.append(Factory.create(node))
+
+    def set_data(self, index: int, data: list):
+        self.nodes[index].data = data
+
+    def remove(self, index):
+        self.nodes.pop(index)
 
     def eval(self):
         for node in self.nodes:
